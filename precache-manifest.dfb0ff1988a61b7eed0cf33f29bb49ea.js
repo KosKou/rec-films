539c5460de6805791932901e2bb285bce1f33370
Taku <koskoudev@gmail.com>
2020-07-13 08:35:18 -0500

    Updates

---
+++ precache-manifest.dfb0ff1988a61b7eed0cf33f29bb49ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed61d2d0e461165fae7552faace709a2",
+    "url": "/rec-films/index.html"
+  },
+  {
+    "revision": "f5646915df2f69a35edb",
+    "url": "/rec-films/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e42f31e6a5279ae25b0a",
+    "url": "/rec-films/static/js/2.bce3ce83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rec-films/static/js/2.bce3ce83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5646915df2f69a35edb",
+    "url": "/rec-films/static/js/main.1f36d569.chunk.js"
+  },
+  {
+    "revision": "559cbc00f2bdce517271",
+    "url": "/rec-films/static/js/runtime-main.605fae62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rec-films/static/media/logo.5d5d9eef.svg"
+  }
+]);
